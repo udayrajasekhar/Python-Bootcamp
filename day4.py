6ab1487daef7664725f1ea6656ca00a51baaedd1
Udaya Rajasekhar <61941773+udayrajasekhar@users.noreply.github.com>
2021-06-15 09:55:50 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,24 @@
+#Day-4 Task
+#Exercise 1
+a=b=c=int(10)
+print("Values of a,b,c are ",a,",",b,",",c)
+a=int(a/10)
+b=int(b*50)
+c=int(c+60)
+print("After Dividing, Multiplying, and Adding")
+print("The values of a,b,c are",a,",",b,",",c)
+#Exercise 2
+name="abcdef"
+print("The name is ", name)
+x=name.replace("c","G")
+print("After replacing the 3rd charactern The name is:",x)
+
+#Exercise 3
+a=int(10)
+b=float(15)
+print("The initial values of a,b are",a,"int type",b,"float type")
+a=float(a)
+b=int(b)
+print("After converting")
+print(a,"in float type")
+print(b,"in int type")
