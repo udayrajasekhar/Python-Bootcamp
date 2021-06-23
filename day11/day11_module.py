b4c76a24b9b0dd218feaa972b04151ebf54a7ab8
Udaya Rajasekhar <61941773+udayrajasekhar@users.noreply.github.com>
2021-06-23 23:15:03 +0530

    Add files via upload

---
+++ day11/day11_module.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+
+def mymodule(List1):
+    print("Content is:")
+    return "Hello"+" "+List1[0]+"\nHello"+" "+List1[1]
+    
+List1=['Uday','Prem']
+
+
---
+++ day11/day11_part_1.py
@@ -0,0 +1,8 @@
+import day11_module as day
+from day11_module import List1
+print("Initial values are:",day.List1)
+
+day.List1[0]='UDAY RAJASEKHAR'
+day.List1[1]='PREM SAI'
+data=day.mymodule(List1)
+print(data)
